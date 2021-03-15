5472fff813b2f583c0840d737cec44d9770660fe
amitz <amitz@cs.huji.ac.il>
2021-03-15 12:17:40 +0200

    Add name and adjectives formatting

--- AnimalsScrapper.py
+++ AnimalsScrapper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import logging
+import re
 from bs4 import BeautifulSoup
 
 REQUEST_RETRIES_NUM = 10
@@ -10,6 +11,10 @@ LOGGER_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 class AnimalsScrapper:
 
     def __init__(self, target_url):
+        """
+        Create an Animal Scrapper object
+        :param target_url: Url to scrap from
+        """
         self._target_url = target_url
         self._animals_dict = {}
         self._logger = logging.getLogger('scrapper')
@@ -17,39 +22,63 @@ class AnimalsScrapper:
         # create console handler with a higher log level
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
-        # create formatter and add it to the handlers
+        # create formatter and add it to the handler
         formatter = logging.Formatter(LOGGER_FORMAT)
         ch.setFormatter(formatter)
-        # add the handlers to the logger
+        # add the handler to the logger
         self._logger.addHandler(ch)
 
     def run(self):
+        """
+        Request data from the url and parse it into a dict
+        :return:
+        """
         raw_content = self._get_raw_html_content()
         self._logger.info("Remote data retrieved from URL successfully")
-        soup = BeautifulSoup(raw_content, 'html.parser')
-        animals_gen = self._generate_animals(soup)
+        animals_gen = self._generate_animals(raw_content)
         self._logger.info("Generating animals")
-        for name, collateral_adjective in animals_gen:
-            if collateral_adjective in self._animals_dict:
-                self._animals_dict[collateral_adjective].append(name)
-            else:
-                self._animals_dict[collateral_adjective] = [name]
+        for name, collateral_adjectives in animals_gen:
+            for collateral_adjective in collateral_adjectives:
+                if collateral_adjective in self._animals_dict:
+                    self._animals_dict[collateral_adjective].append(name)
+                else:
+                    self._animals_dict[collateral_adjective] = [name]
 
     def get_animals(self):
+        """
+        :return: The animals_dict built from the data
+        """
         return self._animals_dict
 
-    def _generate_animals(self, soup):
+    def _generate_animals(self, raw_content):
+        """
+        Parses the animals list in the page:
+        "https://en.wikipedia.org/wiki/List_of_animal_names"
+        sensitive to its format.
+        :param raw_content: content as retrieved from requests
+        :return: A generator that yields a tuple: (name, collateral_adjectives_list)
+        """
+        soup = BeautifulSoup(raw_content, 'html.parser')
+        # Look at the third table in the page
         table = soup.findAll('table')[2]
         table_body = table.find('tbody')
         rows = table_body.find_all('tr')
         for row in rows:
             cols = row.find_all('td')
             if len(cols) != 0:
-                name = cols[0].text
-                collateral_adjective = cols[5].text
-                yield name, collateral_adjective
+                name = re.match("^[A-Za-z ]+", cols[0].text)
+                if name is not None:
+                    name = name.group().rstrip()
+                collateral_adjectives = re.findall("([A-Za-z]+)", cols[5].text)
+                yield name, collateral_adjectives
 
     def _get_raw_html_content(self):
+        """
+        Retrieves content data from the self._target_url
+        Handles timeout exception with a retry loop,
+        will raise the error received on any other error, bad HTTP status or another network error
+        :return: Raw content data from requests retrieved from the url
+        """
         try:
             r = requests.get(self._target_url)
             r.raise_for_status()
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # animal-name-scrapper
+
+Unhandled edge case:
+1. A reference of an animal to another animal in the table. e.g:
+"Pony - See Horse".
+2. 
